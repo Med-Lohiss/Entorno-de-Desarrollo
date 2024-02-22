2e1ea69ecd8e594893efa879d97da388740bd392
Med-Lohiss <150785495+Med-Lohiss@users.noreply.github.com>
2024-02-22 12:46:31 +0100

    Cambiado archivo

--- UD03.4. Boletín de ejercicios JUnit/src/main/java/es/iessoterohernandez/daw/endes/EjerciciosJunit/NoSuchItemException.java
+++
@@ -1,7 +0,0 @@
-package es.iessoterohernandez.daw.endes.EjerciciosJunit;
-
-public class NoSuchItemException extends Exception {
-	public NoSuchItemException(String message) {
-		super(message);
-	}
-}
